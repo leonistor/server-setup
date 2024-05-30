4ef1b633490c20bedebe47fa6ef04c36ae8b003b
Leo Nistor <leo.nistor@gmail.com>
2024-05-30 14:53:03 +0300

    ask sudo pass once in inventory

--- inventory/test.py
+++ inventory/test.py
@@ -1,1 +1,5 @@
-test_hosts = ["192.168.64.2"]
+import getpass
+
+_SUDO_PASSWORD = getpass.getpass(prompt="Sudo password: ")
+
+test_hosts = [("192.168.64.2", {"_use_sudo_password": _SUDO_PASSWORD})]
