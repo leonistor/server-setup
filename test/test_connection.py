c228487830b6d7f0a85134541f7e14ccd50df37e
Leo Nistor <leo.nistor@gmail.com>
2024-05-31 11:36:05 +0300

    ignore ssh warns

--- test/test_connection.py
+++ test/test_connection.py
@@ -1,4 +1,5 @@
 import sys
+import pytest
 
 sys.path.append("../inventory")
 from inventory import test
@@ -6,5 +7,6 @@ from inventory import test
 testinfra_hosts = test.all
 
 
+@pytest.mark.filterwarnings("ignore:Unknown ssh-ed25519")
 def test_leo(host):
     assert host.user("leo").exists
